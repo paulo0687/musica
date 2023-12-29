b7e62d3525dcf0570c906ba918d4d5e91a131676
paulo0687 <paulo0687@hotmail.com>
2023-12-28 21:06:54 -0300

    first commit

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,18 +1,15 @@
-# en apps/users/models.py
-
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 class Perfil(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     imagen = models.ImageField(
         upload_to='users/imagenes',
-        default='users/imagenes/users.jpg'
+        blank=True,
+        null=True
     )
     modificado = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.username 
-
-
-
+        return self.user.username
